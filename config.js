6b82a99f1442ce1128712a497aa12d347f075571
dahangya <275482839@qq.com>
2024-09-20 19:09:14 +0800

    对配置文件进行了分包

---
+++ config.js
@@ -0,0 +1,71 @@
+const config = {
+    defaultStartCoordinates: {
+        x: 20,
+        y: 20
+    },
+    backgrounds: [
+        {
+            name: '格子背景',
+            value: 'grid',
+            url: 'https://file.yboshi.com/%E6%A0%BC%E5%AD%90%E5%9B%BE.png',
+            startCoordinates: {
+                x: 25,
+                y: 25
+            }
+        },
+        {
+            name: '小红书背景',
+            value: 'memo',
+            url: 'https://file.yboshi.com/%E5%B0%8F%E7%BA%A2%E4%B9%A6%E8%83%8C%E6%99%AF.png'
+        },
+        {
+            name: '新背景',
+            value: 'new',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725505416589_amgdsbt9f.png',
+            startCoordinates: {
+                x: 30,
+                y: 30
+            }
+        },
+        {
+            name: '粉色背景',
+            value: 'pink',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725607732456_ql03ire97.png'
+        },
+        {
+            name: '蓝色背景',
+            value: 'blue',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725607734325_lnnnzgok7.png'
+        },
+        {
+            name: '绿色背景',
+            value: 'green',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725607736386_ow6mw8bwe.png'
+        },
+        {
+            name: '黄色背景',
+            value: 'yellow',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725607738327_vwwvygpi4.png'
+        },
+        {
+            name: '紫色背景',
+            value: 'purple',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725607740238_wi1am6ima.png'
+        },
+        {
+            name: '背景9',
+            value: 'bg9',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725608522987_mwoy09rt7.png'
+        },
+        {
+            name: '背景10',
+            value: 'bg10',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725608524550_858mc01w7.png'
+        },
+        {
+            name: '背景11',
+            value: 'bg11',
+            url: 'https://image.songuo.zhieasy.cn/forum/1725608526559_ll4k5eipw.png'
+        }
+    ]
+};
